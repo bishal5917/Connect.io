5dcf3b875a5618ef3adcfdfd285c516da9ece013
bishal5917 <poudelb172@gmail.com>
2023-01-16 20:57:48 +0545

    Conversation Screen Completed !

--- lib/Screens/Message.dart
+++ lib/Screens/Message.dart
@@ -15,23 +15,49 @@ class Message extends StatelessWidget {
       child: Row(
         children: [
           Container(
+            padding: EdgeInsets.all(3),
             decoration: BoxDecoration(
-              border: Border.all(
-                width: 2,
-                color: Colors.blueAccent
-              ),
-              shape: BoxShape.circle, boxShadow: [
-              BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 5)
-            ]),
+                border: Border.all(width: 2, color: Colors.blueAccent),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5)
+                ]),
             child: CircleAvatar(
               backgroundImage:
                   NetworkImage('https://picsum.photos/id/237/200/300'),
-              radius: 30,
+              radius: 36,
             ),
           ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.65,
+            padding: EdgeInsets.only(left: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Levi Ackerman",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
+                    Text("12:30 PM",
+                        style: TextStyle(fontSize: 10, color: Colors.black54))
+                  ],
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Bye See You",
+                    style: TextStyle(fontSize: 15, color: Colors.black54),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );

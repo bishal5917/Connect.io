31dee1797840ab149cbd201e1823b86adbbf00ed
bishal5917 <poudelb172@gmail.com>
2023-01-16 19:29:12 +0545

    Structure changed ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/Screens/Message.dart';
+import 'package:chat_app/Screens/HomeScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,41 +17,9 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Chat App',
-      home: Scaffold(
-        appBar: AppBar(),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Card(
-                color: Color.fromARGB(255, 252, 252, 252),
-                child: Container(child: Message()
-                    ),
-                elevation: 5,
-              ),
-              Card(
-                color: Color.fromARGB(255, 252, 252, 252),
-                child: Container(child: Message()
-                    ),
-                elevation: 5,
-              ),
-                         Card(
-                color: Color.fromARGB(255, 252, 252, 252),
-                child: Container(child: Message()
-                    ),
-                elevation: 5,
-              ),
-                         Card(
-                color: Color.fromARGB(255, 252, 252, 252),
-                child: Container(child: Message()
-                    ),
-                elevation: 5,
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
